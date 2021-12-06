990a933040b13cfca721f2812e9de41cb6fe7b2e
Arvatel <46490296+Arvatel@users.noreply.github.com>
2021-12-07 02:23:49 +0300

    Create generator.py

---
+++ ch.5/ex56/Anastasiia_kuklina/generator.py
@@ -0,0 +1,9 @@
+import random
+
+n = 100
+disc_size = 200
+
+with open("input.txt", "w") as f:
+	print(n, end="\n", file=f)
+	for i in range(0, n):
+		print(random.randint(0, disc_size - 1), end="\n", file=f)
