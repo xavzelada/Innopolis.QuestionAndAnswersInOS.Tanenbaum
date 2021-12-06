e641d2e15854ffd1ebfe8ff63404265e7f02b46d
Arvatel <46490296+Arvatel@users.noreply.github.com>
2021-12-06 19:41:52 +0300

    Delete ch.4/Anastasiia_Kuklina directory

--- ch.4/Anastasiia_Kuklina/solution.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Using echo command to display the header
-echo -e "i-node  HardLinks  Path\n"
-
-# Using find command to find and locate the inodes
-find / -links +1 -printf '%i \t %n \t %p\n' 2>/dev/null | sort -nk1
